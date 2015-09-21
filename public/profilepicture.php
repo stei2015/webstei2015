2b19f0f5de94680b0e5bca57a0d7567dd8543b2f
Nathan Chris <nathanchrs@outlook.com>
2015-09-21 22:57:14 +0700

    custom data directory in config

--- public/profilepicture.php
+++ public/profilepicture.php
@@ -1,8 +1,10 @@
 <?php
 
+require_once(__DIR__.'/../config.php');
+
 $filePath = filter_var($_GET['id'],FILTER_SANITIZE_NUMBER_INT);
 $filePath = preg_replace('/[^(0-9_\(\)\[\]\-.,)]/','',$filePath);
-$filePath = __DIR__.'/../data/profilepictures/'.$filePath;
+$filePath = DATA_DIR.'profilepictures/'.$filePath;
 
 if(!file_exists($filePath)){
 	$filePath = __DIR__.'/img/defaultprofilepicture';
