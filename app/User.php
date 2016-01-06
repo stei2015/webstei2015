27c25d749645f16d34fb3fcc40e6e7909c3c4fe0
Nathan Chris <nathanchrs@outlook.com>
2016-01-06 15:07:21 +0700

    set mass-assignable user properties

--- app/User.php
+++ app/User.php
@@ -6,13 +6,39 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+
+    protected $primaryKey = 'nim';
+    public $incrementing = false;
+    
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'nim', 'username', 'email', 'password',
+        'namalengkap',
+        'namapanggilan',
+        'tempatlahir',
+        'tanggallahir',
+        'sma',
+        'alamatasal',
+        'kotaasal',
+        'provinsiasal',
+        'kodeposasal',
+        'alamatstudi',
+        'kodeposstudi',
+        'hp',
+        'telepondarurat',
+        'email',
+        'emailstudents',
+        'line',
+        'twitter',
+        'facebook',
+        'golongandarah',
+        'riwayatpenyakit',
+        'unit',
+        'bio',
+        'catatan',
     ];
 
     /**
@@ -21,9 +47,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token', 'lastlogin',
+        'password', 'lastlogin',
     ];
 
-    protected $primaryKey = 'nim';
-    public $incrementing = false;
 }
